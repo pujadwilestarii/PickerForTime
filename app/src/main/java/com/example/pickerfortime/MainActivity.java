2064f6f8f9b608514effa9fdf1ca32e8d2ad26bc
puja dwi lestari <pujadwilestari09@gmail.com>
2020-10-27 17:51:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pickerfortime/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.pickerfortime;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+        }
+
+
+        public void showTimePicker(View view) {
+                DialogFragment newFragment = new TimePickerFragment();
+                newFragment.show(getSupportFragmentManager(),"timePicker");
+        }
+
+        public void processTimePickerResult(int hourOfDay, int minute) {
+                String hour_string = Integer.toString(hourOfDay);
+                String minute_string = Integer.toString(minute);
+                // Assign the concatenated strings to timeMessage.
+                String timeMessage = (hour_string + ":" + minute_string);
+
+                Toast.makeText(this, "Time: " + timeMessage,
+                        Toast.LENGTH_SHORT).show();
+        }
+
+}
---
+++ app/src/main/java/com/example/pickerfortime/TimePickerFragment.java
@@ -0,0 +1,34 @@
+package com.example.pickerfortime;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
+import com.example.pickerfortime.MainActivity;
+import java.util.Calendar;
+
+public class TimePickerFragment extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener {
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+        MainActivity activity = (MainActivity) getActivity();
+        activity.processTimePickerResult(hourOfDay, minute);
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+}
